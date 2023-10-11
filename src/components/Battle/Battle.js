05245e5cbf4f09615692d7f1464a060bdd35b5d5
Erich Engler <erich.engler@hotmail.com>
2023-10-11 14:20:10 -0500

    created typed message hook and battleAnnouncer component

--- src/components/Battle/Battle.js
+++ src/components/Battle/Battle.js
@@ -1,7 +1,7 @@
 import { PlayerSummary } from 'components/PlayerSummary';
 import styles from './styles.module.css';
 import { useState } from 'react';
-import { opponentStats, playerStats } from 'shared/characters';
+import { opponentStats, playerStats } from 'shared';
 import { BattleMenu } from 'components/BattleMenu';
 
 export const Battle = () => {
@@ -13,7 +13,7 @@ export const Battle = () => {
     );
 
     return (
-        <div className={styles.main}>
+        <>
             {/* Opponent stats / summary */}
             <div className={styles.opponent}>
                 <div className={styles.summary}>
@@ -26,6 +26,31 @@ export const Battle = () => {
                     />
                 </div>
             </div>
+
+            <div className={styles.characters}>
+                <div className={styles.gameHeader}>
+                    {playerStats.name} vs {opponentStats.name}
+                </div>
+
+                <div className={styles.gameImages}>
+                    <div className={styles.playerSprite}>
+                        <img
+                            alt={playerStats.name}
+                            src={playerStats.img}
+                            // className={styles.}
+                        />
+                    </div>
+
+                    <div className={styles.opponentSprite}>
+                        <img
+                            alt={opponentStats.name}
+                            src={opponentStats.img}
+                            // className={styles.}
+                        />
+                    </div>
+                </div>
+            </div>
+
             {/* Player stats / summary */}
             <div className={styles.user}>
                 <div className={styles.summary}>
@@ -37,15 +62,17 @@ export const Battle = () => {
                         maxHealth={playerStats.maxHealth}
                     />
                 </div>
-            </div>
 
-            <div className={styles.hudChild}>
-                <BattleMenu
-                    onAttack={() => console.log('Attack!')}
-                    onMagic={() => console.log('Magic!')}
-                    onHeal={() => console.log('Heal!')}
-                />
+                <div className={styles.hud}>
+                    <div className={styles.hudChild}>
+                        <BattleMenu
+                            onAttack={() => console.log('Attack!')}
+                            onMagic={() => console.log('Magic!')}
+                            onHeal={() => console.log('Heal!')}
+                        />
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ export * from './Battle';
 export * from './Bar';
 export * from './PlayerSummary';
 export * from './BattleMenu';
+export * from './BattleAnnouncer';
 
 // Export all components here so that
 // Index.js in src folder will only import components
